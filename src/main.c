9a7165b71fb1d465bece944119bf3554e79bd56e
root <root@LAPTOP-SM31145Q>
2024-06-23 19:14:12 -0400

    sub emmissions

--- src/main.c
+++ src/main.c
@@ -82,8 +82,7 @@ typedef struct ColorStop {
 
 typedef enum EMIT_CONDITION {
   EC_LIFE_END,
-  EC_CONTINUOUS,
-  EC_SPAWN,
+  EC_LIFE_START,
 } EMIT_CONDITION;
 
 typedef enum EMIT_TYPE {
@@ -93,8 +92,6 @@ typedef enum EMIT_TYPE {
 } EMIT_TYPE;
 
 typedef struct EmitterConfig { 
-  float origin_x;
-  float origin_y;
   float start_angle_rad;
   float end_angle_rad;
 
@@ -113,7 +110,8 @@ typedef struct EmitterConfig {
   EMIT_CONDITION condition;
   EMIT_TYPE type;
 
-  struct EmitterConfig* children;
+  int sub_emmissions_count;
+  struct EmitterConfig* sub_emmissions;
 } EmitterConfig;
 
 typedef struct Particle {
@@ -123,7 +121,8 @@ typedef struct Particle {
   float y_velocity;
   float travel_time_secs;
   
-  unsigned long emit_running_count;
+  unsigned long emit_count;
+  EmitterConfig* emitting_config;
   EmitterConfig* config;
 } Particle;
 
@@ -131,7 +130,6 @@ typedef struct ParticleSystem {
   Particle particles[MAX_PARTICLES];
   EmitterConfig* config;
   int particle_count;
-
   float origin_x;
   float origin_y;
 } ParticleSystem;
@@ -140,78 +138,97 @@ double rand01() {
   return (double)rand() / RAND_MAX;
 }
 
-void update_particles(EmitterConfig* ps, double delta_time) {
-  for (int i = 0; i < ps->_particle_count; i++) {
-    Particle* particle = &ps->particles[i]; 
+void update_particles(ParticleSystem* ps, double delta_time) {
+  for (int i = 0; i < ps->particle_count; i++) {
+    Particle* particle = &ps->particles[i];
 
-    particle->y_velocity += ps->gravity_force * delta_time;
-
-    particle->x += particle->x_velocity * delta_time * ps->particle_speed;
-    particle->y += particle->y_velocity * delta_time * ps->particle_speed;
     particle->travel_time_secs += delta_time;
+    
+    if (particle->config == NULL) continue;
 
-    if (particle->travel_time_secs > ps->max_lifetime) {
-      Particle temp = ps->particles[ps->_particle_count - 1];
-      ps->particles[ps->_particle_count - 1] = *particle;
+    particle->y_velocity += particle->config->gravity_force * delta_time;
+
+    particle->x += particle->x_velocity * delta_time * particle->config->particle_speed;
+    particle->y += particle->y_velocity * delta_time * ps->config->particle_speed;
+
+    if (particle->travel_time_secs > particle->config->max_lifetime) {
+      Particle temp = ps->particles[ps->particle_count - 1];
+      ps->particles[ps->particle_count - 1] = *particle;
       *particle = temp;
 
-      ps->_particle_count -= 1;
+      ps->particle_count -= 1;
       i -= 1;
-      //avoid skipping the swapped particle
     }
   }
 }
 
-void spawn_particles(EmitterConfig* ps, double delta_time) {
-  double range = ps->start_angle_rad - ps->end_angle_rad; 
+void spawn_particles(ParticleSystem* ps, double delta_time) {
+  for (int i = 0; i < ps->particle_count; i++) {
+    Particle* particle = &ps->particles[i]; 
 
-  ps->_running_time += delta_time;
+    if (particle->emitting_config == NULL) continue;
+    if (particle->travel_time_secs == 0) continue;
 
-  double particles_per_second = ps->_running_count / ps->_running_time;
-  double target_spawn_frequency = ps->spawn_frequency;
+    EmitterConfig* particle_emitting_config = particle->emitting_config;
 
-  while (particles_per_second < target_spawn_frequency) {
-    double angle = rand01() * range + ps->start_angle_rad;
-
-    Particle particle;
+    double range = particle_emitting_config->start_angle_rad - particle_emitting_config->end_angle_rad; 
     
-    particle.travel_time_secs = 0;
-    particle.y_velocity = cos(angle);
-    particle.x_velocity = sin(angle);
-    particle.x = ps->origin_x;
-    particle.y = ps->origin_y;
+    double particles_per_second = particle->emit_count / particle->travel_time_secs;
 
-    ps->particles[ps->_particle_count] = particle; 
+    double target_spawn_frequency = particle->emitting_config->spawn_frequency;
+   
+    while (particles_per_second < target_spawn_frequency) {
+      double angle = rand01() * range + particle->emitting_config->start_angle_rad;
+      int type = rand01() * particle->emitting_config->sub_emmissions_count;
 
-    ps->_particle_count += 1;
-    ps->_running_count += 1;
-    particles_per_second = ps->_running_count / ps->_running_time;    
+      Particle new_particle;
+
+      new_particle.travel_time_secs = 0;
+      new_particle.y_velocity = cos(angle);
+      new_particle.x_velocity = sin(angle);
+      new_particle.x = particle->x;
+      new_particle.y = particle->y;
+      new_particle.emit_count = 0;
+      new_particle.config = particle->emitting_config;
+      new_particle.emitting_config = &particle->emitting_config->sub_emmissions[type];
+
+      ps->particles[ps->particle_count] = new_particle; 
+
+      ps->particle_count += 1;
+      particle->emit_count += 1;
+      
+      particles_per_second = particle->emit_count / particle->travel_time_secs;
+    }
   }
 }
 
-void render_particles(EmitterConfig* ps, SDL_Renderer* renderer, SDL_Texture* particle_image) {
-  const double half_particle_size = ps->particle_size / 2.0f;
-
-  int total = 0;
-
-  for (int i = 0; i < ps->color_stop_count; i++) {
-    total += ps->color_stops[i].ratio;
-  }
-
-  for (int i = 0; i < ps->_particle_count; i++) {
+void render_particles(ParticleSystem* ps, SDL_Renderer* renderer, SDL_Texture* particle_image) {
+  for (int i = 0; i < ps->particle_count; i++) {
     Particle* particle = &ps->particles[i];
+    
+    if (particle->config == NULL) continue;
+
+    EmitterConfig* particle_config = particle->config;
+
+    const double half_particle_size = particle_config->particle_size / 2.0f;
+
+    int total = 0;
+
+    for (int i = 0; i < particle_config->color_stop_count; i++) {
+      total += ps->config->color_stops[i].ratio;
+    }
 
     SDL_Rect render_rect;
     render_rect.x = particle->x - half_particle_size;
     render_rect.y = particle->y - half_particle_size;
-    render_rect.w = ps->particle_size;
-    render_rect.h = ps->particle_size;
+    render_rect.w = half_particle_size * 2.0f;
+    render_rect.h = half_particle_size * 2.0f;
 
-    const double age = particle->travel_time_secs / ps->max_lifetime;
+    const double age = particle->travel_time_secs / particle->config->max_lifetime;
     double min_age = 0;
     
-    for (int j = 0; j < ps->color_stop_count; j++) {
-      const double current_age_addition = ps->color_stops[j].ratio / (double)total;
+    for (int j = 0; j < particle_config->color_stop_count; j++) {
+      const double current_age_addition = particle_config->color_stops[j].ratio / (double)total;
       const double max_age = current_age_addition + min_age;
  
       if (age < min_age || age > max_age) {
@@ -227,38 +244,15 @@ void render_particles(EmitterConfig* ps, SDL_Renderer* renderer, SDL_Texture* pa
       const double max_ratio = scale_amount * max_delta;
       const double min_ratio = scale_amount * min_delta;
 
-      int next = (j+1) % ps->color_stop_count;
+      int next = (j+1) % particle_config->color_stop_count;
       
-      Uint8 red = ps->color_stops[next].r * min_ratio + ps->color_stops[j].r * max_ratio;
-      Uint8 green = ps->color_stops[next].g * min_ratio + ps->color_stops[j].g * max_ratio;
-      Uint8 blue = ps->color_stops[next].b * min_ratio + ps->color_stops[j].b * max_ratio;
-
-      //printf("%f %f\n", max_ratio, min_ratio);
-
-      if (i == 0) {
-        //printf("red: %i green: %i blue: %i\n", 
-        //  ps->color_stops[j].r, 
-        //  ps->color_stops[j].g, 
-        //  ps->color_stops[j].b
-        //);
-
-        //printf("red: %i green: %i blue: %i\n", 
-        //  ps->color_stops[next].r, 
-        //  ps->color_stops[next].g, 
-        //  ps->color_stops[next].b
-        //);
-        //
-
-        printf("min age: %f, age: %f, max age: %f\n", min_age, age, max_age);
-
-        printf("red: %i green: %i blue: %i\n", red, green, blue);
-      
-        printf("\n");
-      }
+      Uint8 red = particle_config->color_stops[next].r * min_ratio + particle_config->color_stops[j].r * max_ratio;
+      Uint8 green = particle_config->color_stops[next].g * min_ratio + particle_config->color_stops[j].g * max_ratio;
+      Uint8 blue = particle_config->color_stops[next].b * min_ratio + particle_config->color_stops[j].b * max_ratio;
 
       SDL_SetTextureColorMod(particle_image, red, green, blue);
 
-      Uint8 alpha = ps->color_stops[next].a * min_ratio + ps->color_stops[j].a * max_ratio;
+      Uint8 alpha = particle_config->color_stops[next].a * min_ratio + particle_config->color_stops[j].a * max_ratio;
 
       SDL_SetTextureAlphaMod(particle_image, alpha); 
 
@@ -273,44 +267,161 @@ void render_particles(EmitterConfig* ps, SDL_Renderer* renderer, SDL_Texture* pa
   }
 }
 
-void update_particle_system(EmitterConfig* ps, SDL_Renderer* renderer, SDL_Texture* particle_image, double delta_time) {
+void update_particle_system(ParticleSystem* ps, SDL_Renderer* renderer, SDL_Texture* particle_image, double delta_time) {
   spawn_particles(ps, delta_time);
   update_particles(ps, delta_time);
   render_particles(ps, renderer, particle_image);
 }
 
-void init_particle_system(EmitterConfig* ps) {
-  //changes at runtime 
-  ps->_particle_count = 0;
-  ps->_running_count = 0;
-  ps->_running_time = 0;
+void print_color_stop(const ColorStop* color_stop) {
+  printf(
+    "r: %i, g: %i b: %i, a: %i\n", 
+    color_stop->r, 
+    color_stop->g, 
+    color_stop->b, 
+    color_stop->a
+  );
+} 
 
+void print_emit_condition(EMIT_CONDITION condition) {
+  switch (condition) {
+    case EC_LIFE_START:
+      printf("EC_LIFE_START\n");
+    break;
+    case EC_LIFE_END:
+      printf("EC_LIFE_END\n");
+    break;
+    default:
+      printf("Unhandled Case\n");
+    break;
+  }
+}
+
+void print_emit_type(EMIT_TYPE type) {
+  switch (type) {
+    case ET_NONE:
+      printf("ET_NONE\n");
+    break;
+    case ET_CONTINOUS:
+      printf("ET_CONTINOUS\n");
+    break;
+    case ET_BURST:
+      printf("ET_BURST\n");
+    break;
+    default:
+      printf("Unhandled Case\n");
+    break;
+  }
+}
+
+void print_emitter_config(const EmitterConfig* ec) {
+  printf("max_lifetime %f\n", ec->max_lifetime);
+  printf("spawn_frequency %f\n", ec->spawn_frequency);
+  printf("start_angle_rad %f\n", ec->start_angle_rad);
+  printf("end_angle_rad %f\n", ec->end_angle_rad);
+  printf("particle_size %i\n", ec->particle_size);
+  printf("particle_speed %f\n", ec->particle_speed);
+  printf("gravity_force %f\n", ec->gravity_force);
+
+  printf("color_stop_count %i\n", ec->color_stop_count);
+
+  for (int i = 0; i < ec->color_stop_count; i++) {
+    printf("index: %i | ", i); 
+    print_color_stop(&ec->color_stops[i]);
+  }
+
+  print_emit_type(ec->type);
+  print_emit_condition(ec->condition);
+
+  printf("child_count %i\n", ec->sub_emmissions_count);
+  
+  for (int i = 0; i < ec->sub_emmissions_count; i++) {
+    printf("\nchild %i\n\n", i);
+    print_emitter_config(&ec->sub_emmissions[i]);
+    printf("\n");
+  }
+}
+
+void init_fireworks_base_config(EmitterConfig* ec) {
   //settings basically
-  ps->origin_x = 600.0f / 2;
-  ps->origin_y = 400.0f / 2;
+  ec->max_lifetime = 10;
 
-  ps->max_lifetime = 20;
-
-  ps->spawn_frequency = 15;
+  ec->spawn_frequency = 5;
   
-  ps->start_angle_rad = 0 - PI / 2;
-  ps->end_angle_rad = PI / 2;
+  ec->start_angle_rad = 0 - PI / 2;
+  ec->end_angle_rad = PI / 2;
 
-  ps->particle_size = 16;
+  ec->particle_size = 16;
   
-  ps->particle_speed = 20;
+  ec->particle_speed = 20;
 
-  ps->gravity_force = 0.0981;
+  ec->gravity_force = 0.0981;
 
   ColorStop a = {1, 255, 255, 255, 255};
   ColorStop b = {1, 0, 180, 225, 125};
   ColorStop c = {1, 0, 0, 255, 0};
 
-  ps->color_stops[0] = a;
-  ps->color_stops[1] = b;
-  ps->color_stops[2] = c;
+  ec->color_stops[0] = a;
+  ec->color_stops[1] = b;
+  ec->color_stops[2] = c;
+
+  ec->condition = EC_LIFE_END;
+  ec->type = ET_CONTINOUS;
   
-  ps->color_stop_count = 4;
+  ec->color_stop_count = 3;
+  ec->sub_emmissions_count = 0;
+  ec->sub_emmissions = NULL;
+}
+
+
+void init_fireworks_recurse_config(EmitterConfig* ec) {
+  //settings basically
+  ec->max_lifetime = 1;
+
+  ec->spawn_frequency = 5;
+  
+  ec->start_angle_rad = 0;
+  ec->end_angle_rad = PI * 2;
+
+  ec->particle_size = 4;
+  
+  ec->particle_speed = 100;
+
+  ec->gravity_force = 0.0981;
+
+  ColorStop a = {1, 255, 255, 255, 255};
+  ColorStop b = {1, 0, 180, 225, 125};
+  ColorStop c = {1, 0, 0, 255, 0};
+
+  ec->color_stops[0] = a;
+  ec->color_stops[1] = b;
+  ec->color_stops[2] = c;
+
+  ec->condition = EC_LIFE_END;
+  ec->type = ET_NONE;
+  
+  ec->color_stop_count = 3;
+  ec->sub_emmissions_count = 0;
+  ec->sub_emmissions = NULL;
+}
+
+void init_particle_system(ParticleSystem* ps, EmitterConfig* ec) {
+  ps->config = ec;
+  ps->particle_count = 1;
+  ps->origin_x = 600 / 2.0f; 
+  ps->origin_y = 400 / 2.0f; 
+
+  Particle root_emitter;
+  root_emitter.x = ps->origin_x;
+  root_emitter.y = ps->origin_y;
+  root_emitter.x_velocity = 0;
+  root_emitter.y_velocity = 0;
+  root_emitter.config = NULL;
+  root_emitter.emitting_config = ps->config;
+  root_emitter.emit_count = 0;
+  root_emitter.travel_time_secs = 0;
+
+  ps->particles[0] = root_emitter;
 }
 
 int main() {
@@ -323,9 +434,18 @@ int main() {
 
   SDL_Texture* texture = load_png_into_texture(renderer, "./sprites/circle.png", true, 16, 16);
 
-  EmitterConfig particle_system;
-  init_particle_system(&particle_system);
+  EmitterConfig emitter_config;
+  EmitterConfig emitter_config_child;
+  init_fireworks_base_config(&emitter_config);
+  init_fireworks_recurse_config(&emitter_config_child);
+  emitter_config.sub_emmissions = &emitter_config_child;
+  emitter_config.sub_emmissions_count = 1;
 
+  ParticleSystem particle_system;
+  init_particle_system(&particle_system, &emitter_config);
+
+  print_emitter_config(particle_system.config);
+  
   srand(time(NULL));
 
   double LAST = (double)SDL_GetPerformanceCounter() / SDL_GetPerformanceFrequency();
@@ -352,9 +472,9 @@ int main() {
 
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
-  
+   
     update_particle_system(&particle_system, renderer, texture, delta_time);
-    
+
     SDL_RenderPresent(renderer);
   }
 
